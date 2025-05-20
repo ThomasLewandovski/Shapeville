67712cc74715dc558cea8bbf60255d5a3551cc32
Khaneason <3136878125@qq.com>
2025-05-21 00:27:30 +0800

    最终final

--- run.bat
+++ run.bat
@@ -1,29 +1,21 @@
 @echo off
-echo === Compiling Java source files ===
+echo === Compiling Shapeville ===
 
-:: 启用变量延迟展开
-setlocal enabledelayedexpansion
-
-:: 设置 classpath，包括 lib 和资源目录
+:: 设置 classpath（lib 可选）
 set CP=.;lib\*;resources
 
-:: 确保 bin 目录存在
+:: 创建 bin 目录
 if not exist bin mkdir bin
 
-:: 清空 bin 中旧的 class 文件
+:: 清空 bin
 del /s /q bin\*.class >nul 2>&1
 
-:: 编译所有 Java 文件
-for /r src %%f in (*.java) do (
-    echo Compiling %%f
-    javac -encoding UTF-8 -d bin -cp "!CP!" "%%f"
-)
+:: 编译：一次性编译所有 Java 文件（重点）
+javac -encoding UTF-8 -cp "%CP%" -d bin @sources.txt
 
-echo === Compilation Finished ===
-
-:: 复制资源到 bin（图片、Archive等）
+:: 复制资源（图片等）
 xcopy /E /Y /I resources bin
 
-echo === Running Shapeville Game ===
+echo === Running Game ===
 java -cp "bin;lib\*" com.shapeville.ui.Main
 pause
